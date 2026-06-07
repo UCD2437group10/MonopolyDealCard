f7d3b963c442bb499d7e07631b898d9eb4386d56
qiutong dong <qiutong.dong@ucdconnect.ie>
2026-06-07 10:45:56 +0800

    view game engine

--- src/main/java/edu/group10/monopolydeal/backend/model/card/SimpleCard.java
+++
@@ -1,8 +0,0 @@
-package edu.group10.monopolydeal.backend.model.card;
-
-/**
- * Simple card implementation
- */
-
-public record SimpleCard (String name, CardType type, String color, int bankValue) implements Card {
-}
--- src/main/java/edu/group10/monopolydeal/backend/model/player/Player.java
+++
@@ -1,8 +0,0 @@
-package edu.group10.monopolydeal.backend.model.player;
-
-/**
- * Player object
- */
-
-public record Player (String id, String displayName, boolean bot) {
-}
